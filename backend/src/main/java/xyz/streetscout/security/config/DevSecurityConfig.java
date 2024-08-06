474b0e452a02fa1957fd77dd94c5213d8a3bc3e0
Devin Catuns <devincatuns1@gmail.com>
2024-08-06 14:37:50 -0400

    refactor: create dev profile security config

--- backend/src/main/java/xyz/streetscout/security/config/DevSecurityConfig.java
+++ backend/src/main/java/xyz/streetscout/security/config/DevSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,37 +16,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import xyz.streetscout.security.CustomuserDetailsService;
 
-@Profile("prod")
+@Profile("!prod")
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
-public class SecurityConfig {
+public class DevSecurityConfig {
 
     @Autowired
     private CustomuserDetailsService customUserDetailsService;
-    @Autowired
-    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(
-                                "/auth/**",
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/api/admin/**").hasAnyAuthority("ADMIN")
-                        .requestMatchers("/api/vendors/**").hasAnyAuthority("VENDOR","ADMIN")
-                        .requestMatchers("/api/customers/**").hasAnyAuthority("CUSTOMER","ADMIN")
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .authenticationProvider(authenticationProvider());
 
         return httpSecurity.build();
     }
