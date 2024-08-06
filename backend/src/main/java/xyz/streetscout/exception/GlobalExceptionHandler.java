98ee66aaffd7d9182a2b44d3cb7a371ab44c5caa
Devin Catuns <devincatuns1@gmail.com>
2024-08-06 17:48:54 -0400

    feat: add default exception handler

---
+++ backend/src/main/java/xyz/streetscout/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package xyz.streetscout.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> genericExceptionHandler(
+            Exception e,
+            HttpServletRequest request
+    ) {
+        String message = String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage());
+        log.error(message);
+        ErrorMessage errorMessage = new ErrorMessage(
+                request.getRequestURI(),
+                message,
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundExceptionHandler(
+            EntityNotFoundException e,
+            HttpServletRequest request
+    ) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.NOT_FOUND,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+}
